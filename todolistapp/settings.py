cc7792f8e220513d545e471684f5bf280710c646
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2023-09-06 00:59:55 +0100

    Setup Postgres database

--- todolistapp/settings.py
+++ todolistapp/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,11 +90,13 @@ WSGI_APPLICATION = 'todolistapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    "default": dj_database_url.parse(os.getenv("DATABASE_URL"), conn_max_age=600),
 }
+
 
 
 # Password validation
